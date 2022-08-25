9b24224e28ddd70481374e2f67a7d81c03765978
David Jiménez <skyrroner@gmail.com>
2022-08-25 12:05:50 -0500

    Total en balance service

--- src/controllers/balance/balance.service.ts
+++ src/controllers/balance/balance.service.ts
@@ -5,39 +5,48 @@ import { FirestorageService } from '../firestorage/firestorage.service';
 @Injectable()
 export class BalanceService {
 
-    constructor(
-        private readonly balanceRepository: BalanceRepository,
-        private readonly imageRepository: ImageRepository,
-        private firestorageService: FirestorageService
-    ) {}
+   constructor(
+      private readonly balanceRepository: BalanceRepository,
+      private readonly imageRepository: ImageRepository,
+      private firestorageService: FirestorageService
+   ) { }
 
-    async create(request: any){
+   async create(request: any) {
       const balance = await this.balanceRepository.create(request)
-      if (!balance) throw new HttpException('incorrect data',HttpStatus.BAD_REQUEST)   
+      if (!balance) throw new HttpException('incorrect data', HttpStatus.BAD_REQUEST)
       return balance;
    }
 
-   async getAll(id){
-      const balances = await this.balanceRepository.getAll(id)
+   async getAll(id) {
+      let balances = await this.balanceRepository.getAll(id) 
+      balances.unshift(this.totalAmount(balances))
       return balances;
    }
 
-   async getById(id:number){
+   async getById(id: number) {
       const balance = await this.balanceRepository.getById(id)
       return balance;
    }
 
-   async update(id:number, request: any){
+   async totalAmount(balances) {
+      let total
+      for (let balance of balances) {
+         total += balance.amount
+      }
+      return total;
+   }
+
+   async update(id: number, request: any) {
 
       const balance = await this.balanceRepository.update(id, request)
 
       return balance;
    }
 
-   async delete(id: number){
+   async delete(id: number) {
       const balance = await this.balanceRepository.delete(id)
 
-      return {statusCode: 200, message: 'removed'}
-   }  
+      return { statusCode: 200, message: 'removed' }
+   }
 
 }
