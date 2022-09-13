b764b900be3845646c1a903da299ffc358ea2537
David Jiménez <skyrroner@gmail.com>
2022-09-13 12:43:15 -0500

    add title in offers

--- src/modules/database/repositories/offerRepository.service.ts
+++ src/modules/database/repositories/offerRepository.service.ts
@@ -23,7 +23,7 @@ export class OfferRepository {
     async getAll(career?, search?): Promise<Offer[] | string> {
         let query = 'o.offer_category_id != 1 AND o.offer_category_id != 2'
         career ? query += ` AND o.career_id = ${career}` : ''
-        search ? query += ` AND (o.description LIKE '%${search}%')` : ''
+        search ? query += ` AND (o.description LIKE '%${search}%' OR o.title LIKE '%${search}%')` : ''
 
         return await this.offersRepository.createQueryBuilder('o')
             .innerJoinAndSelect('o.offerCategory', 'oo')
