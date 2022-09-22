4cdacbacf81c184140a93234b57cec13d6cbab0f
David Jiménez <skyrroner@gmail.com>
2022-09-22 17:40:13 -0500

    Changes and fixes

--- src/controllers/subjectCategory/subjectCategory.service.ts
+++ src/controllers/subjectCategory/subjectCategory.service.ts
@@ -4,79 +4,95 @@ import { SubjectCategoryRepository } from '../../modules/database/repositories/s
 @Injectable()
 export class SubjectCategoryService {
 
-    constructor(
-        private readonly subjectCategoryRepository: SubjectCategoryRepository,
-        private readonly userSubjectRepository: UserSubjectRepository
-    ) {}
+   constructor(
+      private readonly subjectCategoryRepository: SubjectCategoryRepository,
+      private readonly userSubjectRepository: UserSubjectRepository
+   ) { }
 
-    async create(request: any){
+   async create(request: any) {
 
       const subjectCategory = await this.subjectCategoryRepository.create(request)
-      if (!subjectCategory) throw new BadRequestException(['incorrect data'])     
+      if (!subjectCategory) throw new BadRequestException(['incorrect data'])
 
       return subjectCategory;
    }
 
-   async getAll(data, id){
+   async getAll(data, id) {
       let subjectCategorys = await this.subjectCategoryRepository.getAll(data, id)
-      if(data.userData.userRole[0].role_id == 2){
+      if (data.userData.userRole[0].role_id == 2) {
          subjectCategorys = await this.addStudentExtraInfo(data.userData, subjectCategorys)
       }
       return subjectCategorys;
    }
 
-   async getById(id:number){
+   async getById(id: number) {
       const subjectCategory = await this.subjectCategoryRepository.getById(id)
       return subjectCategory;
    }
 
-   async update(id:number, request: any){
+   async update(id: number, request: any) {
 
       const subjectCategory = await this.subjectCategoryRepository.update(id, request)
 
       return subjectCategory;
    }
 
-   async delete(id: number){
+   async delete(id: number) {
       const subjectCategory = await this.subjectCategoryRepository.delete(id)
 
 
-      return {statusCode: 200, message: 'removed'}
+      return { statusCode: 200, message: 'removed' }
    }
 
    async addStudentExtraInfo(userData, data) {
-      let res = {data: null, total: null, on: null, prom: null}
+      let res = { data: null, total: null, on: null, prom: null, correlations: null }
       let on = 0
       let points = 0
       let count = 0
       let total = 0
-      
+      let correlations = []
+
       let userSubjects = await this.userSubjectRepository.getAll(userData.id, userData.career_id)
-         for (let i = 0; i < data.length; i++) {
-            for (let j = 0; j < data[i].subject.length; j++) {
-               total++
-               data[i].subject[j].userSubject = userSubjects.find(a => a.subject_id == data[i].subject[j].id)
-               if (data[i].subject[j].userSubject) {
-                  if(data[i].subject[j].userSubject.score>=4) {
-                     on++
-                  }
-                  if(data[i].subject[j].userSubject.score) {
-                     count++
-                     points += data[i].subject[j].userSubject.score
-                  }
+      for (let i = 0; i < data.length; i++) {
+         for (let j = 0; j < data[i].subject.length; j++) {
+            total++
+            data[i].subject[j].userSubject = userSubjects.find(a => a.subject_id == data[i].subject[j].id)
+            if (data[i].subject[j].userSubject) {
+               if (data[i].subject[j].userSubject.score >= 4) {
+                  on++
+               }
+               if (data[i].subject[j].userSubject.score) {
+                  count++
+                  points += data[i].subject[j].userSubject.score
                }
             }
+            if (data[i].subject[j].subject_id) {
+               let subject_id = data[i].subject[j].subject_id
+               let subjectData
+               subjectData = data.map(category => category.subject.find(subject => subject.id == subject_id))
+               subjectData.map(subject => {
+                  if (subject) {
+                     if (subject.userSubject.finish && subject.userSubject.score >= 4) {
+                        correlations.push({ mainSubject_id: subject.id, mainName: subject.name, subject_id: data[i].subject[j].id, name: data[i].subject[j].name, available: true })
+                     } else {
+                        correlations.push({ mainSubject_id: subject.id, mainName: subject.name, subject_id: data[i].subject[j].id, name: data[i].subject[j].name, available: false })
+                     }
+                  }
+               })
+            }
          }
-      
+      }
+
       if (points && count) {
-         res.prom = parseFloat((points / count).toFixed(2))   
+         res.prom = parseFloat((points / count).toFixed(2))
       }
       res.total = total
       res.on = on
       res.data = data
+      res.correlations = correlations
       return res
    }
 
-     
+
 
 }
